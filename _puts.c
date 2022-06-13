d29eb5e2fa1c4ddf5948111be54295c388440c90
mmoh-i <ibrahimmusty100@yahoo.com>
2022-06-13 10:44:53 -0700

    puts function

---
+++ _puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *_puts - prints a string
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	int i;
+	i = 0;
+	while (str[i])
+	{
+		_putschar(str[i]);
+		i++;
+	}
+}
